be9f54cf97a509dae2feb90ec067a0acd75c26fe
tnd18091995 <tnd18091995@gmail.com>
2024-03-28 15:25:26 +0700

    emmailtest

---
+++ src/EmailEx/Main.java
@@ -0,0 +1,19 @@
+package EmailEx;
+
+public class Main {
+    private static EmailTest emailExample;
+    public static final String[] validEmail = new String[]{"a@gmail.com", "ab@yahoo.com", "abc@hotmail.com"};
+    public static final String[] invalidEmail = new String[]{"@gmail.com", "ab@gmail.", "@#abc@gmail.com"};
+
+    public static void main(String args[]) {
+        emailExample = new EmailTest();
+        for (String email : validEmail) {
+            boolean isvalid = emailExample.validate(email);
+            System.out.println("Email is " + email + " is valid: " + isvalid);
+        }
+        for (String email : invalidEmail) {
+            boolean isvalid = emailExample.validate(email);
+            System.out.println("Email is " + email + " is valid: " + isvalid);
+        }
+    }
+}
